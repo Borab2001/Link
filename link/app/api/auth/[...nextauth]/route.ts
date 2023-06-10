e4a965bff498f8f315e2d9fc125c7fa966240d8b
Bora <borabalos9@gmail.com>
2023-06-10 20:09:05 +0200

    Next auth setup done

--- link/app/api/auth/[...nextauth]/route.ts
+++ link/app/api/auth/[...nextauth]/route.ts
@@ -24,7 +24,41 @@ export const authOptions: AuthOptions = {
                 email: { label: 'email', type: 'text' },
                 password: { label: 'password', type: 'password' },
             },
-            
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Invalid Credentials');
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user || !user?.hashedPassword) {
+                    throw new Error('Invalid credentials');
+                }
+
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+
+                if (!isCorrectPassword) {
+                    throw new Error('Invalid credentials');
+                }
+
+                return user;
+            }
         })
-    ]
-}
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
