bed5bcb174c4d653bc2cf582c177176581206a5a
Bora <borabalos9@gmail.com>
2023-06-17 17:15:51 +0200

    OAuth finished + prisma db fix

--- link/app/(site)/components/AuthForm.tsx
+++ link/app/(site)/components/AuthForm.tsx
@@ -69,7 +69,17 @@ const AuthForm = () => {
     const socialAction = (action: string) => {
         setIsLoading(true);
 
-        //NextAuth Social Sign In
+        signIn(action, { redirect: false })
+        .then((callback) => {
+            if (callback?.error) {
+                toast.error('Invalid Credentials');
+            }
+
+            if (callback?.ok && !callback?.error) {
+                toast.success('Logged in!')
+            }
+        })
+        .finally(() => setIsLoading(false));
     }
 
     return (
