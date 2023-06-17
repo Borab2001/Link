4b1cdd8700d08b927118487b0c40009ab533a59e
Bora <borabalos9@gmail.com>
2023-06-17 18:29:30 +0200

    Sign in & Sign up pushing to /users

--- link/app/(site)/components/AuthForm.tsx
+++ link/app/(site)/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useState } from "react";
+import { use, useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGoogle, BsGithub } from "react-icons/bs";
 
@@ -9,13 +9,22 @@ import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/users');
+        }
+    }, [session?.status, router]);
 
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN') {
@@ -44,8 +53,12 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             axios.post('/api/register', data)
+            .then(() => signIn('credentials', data))
             .catch(() => toast.error('Oops...Something went wrong!'))
-            .finally(() => setIsLoading(false))
+            .finally(() => {
+                setIsLoading(false);
+                toast.success('Registered successfully!')
+            });
         }
 
         if (variant === 'LOGIN') {
@@ -60,6 +73,7 @@ const AuthForm = () => {
 
                 if (callback?.ok && !callback?.error) {
                     toast.success('Logged in!');
+                    router.push('/users');
                 }
             })
             .finally(() => setIsLoading(false));
---
+++ link/app/users/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { signOut } from "next-auth/react";
+
+const Users = () => {
+    return (
+        <button onClick={() => signOut()}>
+            Sign Out
+        </button>
+    );
+}
+ 
+export default Users;
