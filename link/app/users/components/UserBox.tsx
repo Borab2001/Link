9ced0588884099826f50db336484b3c438ffcf83
Bora <borabalos9@gmail.com>
2023-06-20 12:18:58 +0200

    UserList with UserBox components

---
+++ link/app/users/components/UserBox.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import axios from "axios";
+import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import Avatar from "@/app/components/Avatar";
+
+interface UserBoxProps {
+    data: User
+}
+
+const UserBox: React.FC<UserBoxProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleClick = useCallback(() => {
+        setIsLoading(true);
+
+        axios.post('/api/conversations', {
+            userId: data.id
+        })
+        .then((data) => {
+            router.push(`/conversations/${data.data.id}`);
+        })
+        .finally(() => setIsLoading(false));
+    }, [data, router]);
+
+    return (
+        <div
+            onClick={handleClick}
+            className="
+                w-full
+                relative
+                flex
+                items-center
+                space-x-3
+                bg-white
+                dark:bg-zinc-800
+                p-3
+                hover:bg-neutral-100
+                dark:hover:bg-zinc-900
+                rounded-lg
+                transition
+                cursor-pointer
+            "
+        >
+            <Avatar user={data} />
+            <div className="min-w-0 flex-1">
+                <div className="focus:outline-none">
+                    <div className="flex justify-between items-center mb-1">
+                        <p className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                            {data.name}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default UserBox;
--- link/app/users/layout.tsx
+++ link/app/users/layout.tsx
@@ -1,4 +1,6 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 
 export default async function UsersLayout({
@@ -6,10 +8,12 @@ export default async function UsersLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const users = await getUsers();
     return (
         // @ts-expect-error Server Component
         <Sidebar>
             <div className="h-full">
+                <UserList items={users} />
                 {children}
             </div>
         </Sidebar>
