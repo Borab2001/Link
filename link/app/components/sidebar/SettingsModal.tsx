a116e4515137978b5825bec8958ee81dc1f325b4
Bora <borabalos9@gmail.com>
2023-06-25 23:28:27 +0300

    SettingsModal UI done

--- link/app/components/sidebar/SettingsModal.tsx
+++ link/app/components/sidebar/SettingsModal.tsx
@@ -2,10 +2,15 @@
 
 import { User } from "@prisma/client";
 import axios from "axios";
+import { CldUploadButton } from "next-cloudinary";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import Button from "../Button";
+import Input from "../inputs/Input";
+import Modal from "../Modal";
 
 interface SettingsModalProps {
     isOpen?: boolean;
@@ -57,9 +62,120 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
     }
 
     return (
-        <div>
-            Settings
-        </div>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 dark:border-gray-100/10 pb-12">
+                        <h2 
+                            className="
+                                text-base
+                                font-semibold
+                                leading-7
+                                text-gray-900
+                                dark:text-gray-100
+                            "
+                        >
+                            Profile
+                        </h2>
+                        <p
+                            className="
+                                mt-1
+                                text-sm
+                                leading-6
+                                text-gray-600
+                                dark:text-gray-400
+                            "
+                        >
+                            Edit your public information.
+                        </p>
+
+                        <div
+                            className="
+                                mt-10
+                                flex
+                                flex-col
+                                gap-y-8
+                            "
+                        >
+                            <Input
+                                disabled={isLoading}
+                                label="Name"
+                                id="name"
+                                errors={errors}
+                                required
+                                register={register}
+                            />
+                            <div>
+                                <label
+                                    className="
+                                        block
+                                        text-sm
+                                        font-medium
+                                        leading-6
+                                        text-gray-900
+                                        dark:text-gray-100
+                                    "
+                                >
+                                    Photo
+                                </label>
+                                <div
+                                    className="
+                                        mt-2
+                                        flex
+                                        items-center
+                                        gap-x-3
+                                    "
+                                >
+                                    <Image
+                                        width="48"
+                                        height="48"
+                                        className="rounded-full"
+                                        src={image || currentUser?.image || '/images/placeholder.webp'}
+                                        alt="Avatar"
+                                    />
+                                    <CldUploadButton
+                                        options={{ maxFiles: 1 }}
+                                        onUpload={handleUpload}
+                                        uploadPreset="e1pnfmox"
+                                    >
+                                        <Button
+                                            disabled={isLoading}
+                                            secondary
+                                            type="button"
+                                        >
+                                            Change
+                                        </Button>
+                                    </CldUploadButton>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="
+                            mt-6
+                            flex
+                            items-center
+                            justify-end
+                            gap-x-6
+                        "
+                    >
+                        <Button
+                            disabled={isLoading}
+                            secondary
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            disabled={isLoading}
+                            type="submit"
+                        >
+                            Save
+                        </Button>
+                    </div>
+                </div>
+            </form>
+        </Modal>
     );
 }
  
