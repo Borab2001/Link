d67473445ee7d141dbab858f9bbd6f66807a2b84
Bora <borabalos9@gmail.com>
2023-06-26 17:07:45 +0300

    Loading Modals added w/ react-spinners

--- link/app/users/components/UserBox.tsx
+++ link/app/users/components/UserBox.tsx
@@ -5,6 +5,7 @@ import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 
 interface UserBoxProps {
     data: User
@@ -29,35 +30,40 @@ const UserBox: React.FC<UserBoxProps> = ({
     }, [data, router]);
 
     return (
-        <div
-            onClick={handleClick}
-            className="
-                w-full
-                relative
-                flex
-                items-center
-                space-x-3
-                bg-white
-                dark:bg-neutral-950
-                p-3
-                hover:bg-neutral-100
-                dark:hover:bg-zinc-900
-                rounded-lg
-                transition
-                cursor-pointer
-            "
-        >
-            <Avatar user={data} />
-            <div className="min-w-0 flex-1">
-                <div className="focus:outline-none">
-                    <div className="flex justify-between items-center mb-1">
-                        <p className="text-sm font-medium text-gray-900 dark:text-gray-100">
-                            {data.name}
-                        </p>
+        <>
+            {isLoading && (
+                <LoadingModal />
+            )}
+            <div
+                onClick={handleClick}
+                className="
+                    w-full
+                    relative
+                    flex
+                    items-center
+                    space-x-3
+                    bg-white
+                    dark:bg-neutral-950
+                    p-3
+                    hover:bg-neutral-100
+                    dark:hover:bg-zinc-900
+                    rounded-lg
+                    transition
+                    cursor-pointer
+                "
+            >
+                <Avatar user={data} />
+                <div className="min-w-0 flex-1">
+                    <div className="focus:outline-none">
+                        <div className="flex justify-between items-center mb-1">
+                            <p className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                                {data.name}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
  
