85bf32b0e24daabd1d777a8741fd35bb37329030
Bora <borabalos9@gmail.com>
2023-07-24 03:19:01 +1000

    @ts-ignore for /users layout

--- link/app/users/layout.tsx
+++ link/app/users/layout.tsx
@@ -10,7 +10,7 @@ export default async function UsersLayout({
 }) {
     const users = await getUsers();
     return (
-        // @ts-expect-error Server Component
+        // @ts-ignore Server Component
         <Sidebar>
             <div className="h-full">
                 <UserList items={users} />
